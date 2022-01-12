2ea8da22b9179e2fd27dae014344ddcfe4ce6ca3
ziro <palembani@gmail.com>
2022-01-12 09:25:35 +0700

    + Idk why but oh well

--- js/app.js
+++ js/app.js
@@ -1,95 +1,32 @@
+// ESLint hates this way of using Framework7 ._.
+// eslint-disable-next-line no-undef, no-unused-vars
 const app = new Framework7({
-    // App's root element
     el: "#app",
-    // App's Name
     name: "My App",
-    // App's id
     id: "com.myapp.test",
-    // Enable swipe panel
     panel: {
         swipe: true,
     },
-    // App's routes
+    // eslint-disable-next-line no-undef
     routes: routes // located in js/route.js
 });
 
-const mainView = app.views.create(".view-main");
+// const mainView = app.views.create(".view-main");
+// eslint-disable-next-line no-undef
 const $$ = Dom7;
 
-// $$(document).on("click", "#btn-register", function () {
-$$(document).on("click", "#btn-register", () => {
-    mainView.router.navigate("/register/");
-});
+$$(document).on("click", "#calculate", () => {
+    const x = parseInt(document.getElementById("valX").value);
+    const y = parseInt(document.getElementById("valY").value);
+    const chosen = document.getElementById("choice").value;
 
-$$(document).on("click", "#btn-login", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    app.request({
-        url: "http://127.0.0.1/php/check-login.php",
-        type: "POST",
-        data: {
-            "email": email,
-            "password": password,
-        },
-        dataType: "json",
-        success: (data) => {
-            console.log(data);
-            mainView.router.navigate("/dashboard/");
-        },
-        error: (err) => {
-            // console.log(err.response.msg);
-            app.dialog.alert(JSON.parse(err.response).msg);
-        },
-    })
-});
-
-$$(document).on("click", "#btn-exit", () => {
-    mainView.router.navigate("/");
-});
-
-$$(document).on("click", "#btn-register-commit", () => {
-    let formData = new FormData();
-
-    const file = document.getElementById("photo").files[0];
-    formData.append("file", file);
-
-    const name = document.getElementById("name").value;
-    formData.append("name", name);
-
-    const gradYear = document.getElementById("gradYear").value;
-    formData.append("gradYear", gradYear);
-
-    const prodi = document.getElementById("prodi").value;
-    formData.append("prodi", prodi);
-
-    const email = document.getElementById("regEmail").value;
-    formData.append("email", email);
-
-    const gender = document.getElementById("gender").value;
-    formData.append("gender", gender);
-
-    const password = document.getElementById("regPassword").value;
-    formData.append("password", password);
-
-    if (!!file) {
-        const xhttp = new XMLHttpRequest();
-        xhttp.onload = () => {
-            app.dialog.alert("Berhasil Disimpan !");
-        };
-        xhttp.open("POST", "http://127.0.0.1/php/save-alumni.php", true);
-        xhttp.send(formData);
-    } else {
-        app.dialog.alert("Foto tidak boleh kosong !");
+    let result = document.getElementById("result");
+    switch (chosen) {
+        case "*": result.value = x*y; break;
+        case "+": result.value = x+y; break;
+        case "-": result.value = x-y; break;
+        case "/": result.value = x/y; break;
+        default: alert("how?"); break;
     }
-});
 
-// has to be function () instead of () => {}
-// cuz Dom7 hates it
-$$(document).on("click", "#detail", function ()
-{
-    const index = $$(this).data("index");
-    localStorage.removeItem("index");
-    localStorage.setItem("index", index);
-    mainView.router.navigate("/detail-alumni/");
 });
